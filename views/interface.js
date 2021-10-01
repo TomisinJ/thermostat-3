c5cc613fa3d479e120f0022e0d438ecc3d2eded7
tj <tomisinjoseph3@gmail.com>
2021-10-01 18:39:46 +0100

    used weather API

--- views/interface.js
+++ views/interface.js
@@ -1,10 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const selectElement = document.querySelector('#current-city');
+  selectElement.addEventListener('change', (event) => {
+    const city = event.target.value;
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric`
+
+    fetch(url)
+      .then((response) => {
+        return response.json()
+      })
+      .then((data) => {
+        document.querySelector('#current-temperature').innerText = data.main.temp;
+      })
+    });
 
   const thermostat = new Thermostat();
 
   const updateTemperature = () => {
     document.querySelector('.temp-monitor').innerText = thermostat.getCurrentTemperature();
-    // document.querySelector('.temp-monitor').className = thermostat.checkEnergyUsage();
+    document.querySelector('.temp-monitor').className = "temp-monitor " + thermostat.checkEnergyUsage();
   }
 
   updateTemperature();
